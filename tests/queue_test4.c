fa005e2b1ea657fc8780737431b0a7cb74f9905c
Khaidar Kairbek <khaidarkairbek@proton.me>
2025-10-02 17:25:16 -0400

    add qclose and qsearch tests

--- tests/queue_test4.c
+++ tests/queue_test4.c
@@ -5,6 +5,18 @@
  * Created: 10-02-2025
  * Version: 1.0
  * 
- * Description: 
+ * Description: tests qclose() 
  * 
  */
+#include <stdlib.h>
+#include "queue.h"
+
+int main(void) {
+	queue_t *q = qopen();
+	
+	if (q == NULL) exit(EXIT_FAILURE);
+
+	qclose(q);
+
+	exit(EXIT_SUCCESS); 
+}
